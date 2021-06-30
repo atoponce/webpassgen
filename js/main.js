d88d9479e568c684b8433f35661f17198e857530
Aaron Toponce <aaron.toponce@gmail.com>
2021-06-30 10:07:01 -0600

    fill the svg with the current color

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function setDarkTheme () {
   PAGECONTAINER.classList.add('dark-theme')
   localStorage.setItem('theme', 'dark')
   THEMESWITCHER.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun">
       <circle cx="12" cy="12" r="5"></circle>
       <line x1="12" y1="1" x2="12" y2="3"></line>
       <line x1="12" y1="21" x2="12" y2="23"></line>
@@ -32,7 +32,7 @@ function setLightTheme () {
   PAGECONTAINER.classList.remove('dark-theme')
   localStorage.setItem('theme', 'light')
   THEMESWITCHER.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon">
       <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
     </svg>
   `
