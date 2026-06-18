973789a77ab019d76e0400d72f2a7f7c5fdfd390
Aaron Toponce <aaron.toponce@gmail.com>
2026-06-18 13:06:24 -0600

    Rework the backronym generator (previously "Acronyms")
    My original algorithm would pick a random word to use as the backronym first, then build the
    passphrase from it. Because whe backronym can be any arrangement of characters, I don't know the
    total keyspace the resulting passphrase will come from, even if I already know the length of the
    backronym. So i pre-computed a table of backronym lengths and generated a random 50,000 passphrases
    from backronyms of increasing length. Then I could set the right security contraints on the
    backronym to ensure I'm hitting the desired security target.
    
    The problem with this approach is that I was blind to the keyspace that the password would be coming
    from. So instead, this commit fixes that.
    
    Two buckets are built from the compiled word list: "byLetter" which is sorted by beginning
    character for each word, ignoring case, "a" through "z", and "byLength" which is sorted in asceding
    order by length of each word.
    
    Both buckets are 2-dimensional arrays:
    
        byLetter = [
            ["a", "alice", ...], // words starting with "a"
            ["b", "bob", ...], // words starting with "b"
            ["c", "carol", ...], // words starting with "c"
            ...
        ]
    
        byLength = [
            ["a", "A", "b", ...], // 1-character words
            ["aa", "AA", "bb", ...], // 2-character words
            ["aaa", "AAA", "bbb", ...], // 3-character words
            ...
        ]
    
    Two key space totals are kept: per word and running sum for that length:
    
        keyspace("pass") = count("p") × count("a") × count("s") × count("s")
        total(4-char word) = keyspace("pass") + keyspace("past") + keyspace("pats") + ...
    
    The total sum is kept in an array, where word length is match to index-1. At this point, I know the
    total key space for each byLength words. Thus, when the user selects an entropy requirement, I can
    find the correct word length index based on this total key space.
    
    From there, pick a random n-character word for the right key space and build the passphrase from
    there. The lengths will be the following:
    
    | Bits | Backronym | Passphrase |
    | :--- | :-------: | :--------: |
    |  48  |  4 chars  |  variable  |
    |  56  |  4 chars  |  variable  |
    |  64  |  5 chars  |  variable  |
    |  72  |  5 chars  |  variable  |
    |  80  |  6 chars  |  variable  |
    |  88  |  7 chars  |  variable  |
    |  96  |  8 chars  |  variable  |
    | 104  |  8 chars  |  variable  |
    | 112  |  9 chars  |  variable  |
    | 120  | 10 chars  |  variable  |
    | 128  | 10 chars  |  variable  |

--- js/alternate.js
+++ js/alternate.js
@@ -26,7 +26,7 @@ function generateAlternate(selection) {
     wordList = alternateHR
   } else if (selection === 'Elvish') {
     wordList = alternateElvish
-  } else if (selection === 'Acronyms' || selection === 'Every Word List') {
+  } else if (selection === 'Backronyms' || selection === 'Every Word List') {
     wordList = Object.keys(alternateColors)           //  1029 words
     wordList = wordList.concat(alternateEyeware)      //  8192 words
     wordList = wordList.concat(alternateObscure)      // 19392 words
@@ -54,7 +54,7 @@ function generateAlternate(selection) {
     wordList = wordList.concat(effStarWars)           //  4000 words
     wordList = wordList.concat(moneroEN)              //  1626 words
     wordList = wordList.concat(pseudoDibels)          //  3215 words
-    if (selection === 'Acronyms') {
+    if (selection === 'Backronyms') {
       // Ensure only alphabetic characters.
       wordList = wordList.filter(element => /^[a-z]+$/gi.test(element))
     }
@@ -112,40 +112,26 @@ function generateAlternate(selection) {
   const entropy = getEntropy()
   const len = Math.ceil(entropy / Math.log2(wordList.length))
 
-  if (selection === 'Acronyms') {
-    /**
-     * This is a hack to prevent wasting too much time iterating through the
-     * generation process. These minimum word counts were found via generation
-     * of 5,000 passphrases at each of the different entropy levels and counting
-     * the results.
-     *
-     * See https://gist.github.com/atoponce/317d8a697b685b73d8fa31b2c0615d05
-     */
-    let minWords = (entropy >> 3) - 2
+  if (selection === 'Backronyms') {
+    const results = generateBackronym(wordList, altProps.entropyCheck.checked)
 
-    if ([80, 88, 96, 104].indexOf(entropy) > -1) {
-      minWords -= 1
-    } else if ([112, 120, 128].indexOf(entropy) > -1) {
-      minWords -= 2
-    }
+    const backronym = results.backronym.split('').map(word => {
+      return `<p>${word}</p>`;
+    }).join('.') + '.';
 
-    let results
-    do {
-      results = generateAcronym(minWords, wordList, altProps.entropyCheck.checked)
-      minWords++
-    } while (results.security < entropy)
+    // Put generatePass() or generatePassphrase() in generateBackronym?
+    pass = results.passphrase.map(word => { return `<p>${word}</p>`; }).join('-');
 
-    pass = results.passphrase.trim()
-    altProps.passId.classList.add('acronym')
+    altProps.passId.classList.add('backronym')
     altProps.passId.classList.remove('colors')
-    altProps.passId.innerHTML = pass
+    altProps.passId.innerHTML = backronym + '<br/>' + pass
     altProps.passEntropy.innerText = results.security + ' bits'
     altProps.passLength.innerText = pass.replace(/<\/?span>/g, '').length + ' characters'
     altProps.setSize.innerText = wordList.length.toLocaleString() + ' words'
   } else {
     pass = generatePass(len, wordList, true, altProps.entropyCheck.checked).trim()
     pass = pass.replace(/ /g, '-')
-    altProps.passId.classList.remove('acronym')
+    altProps.passId.classList.remove('backronym')
     altProps.passId.classList.remove('colors')
     altProps.passEntropy.innerText = Math.floor(len * Math.log2(wordList.length)) + ' bits'
     altProps.passId.innerText = pass
@@ -155,53 +141,125 @@ function generateAlternate(selection) {
 }
 
 /**
- * Generate a passphrase built up from an acronym.
- * There is some discussion that my approach might not be secure. A Mastodon
- * discussion about it: https://fosstodon.org/deck/@atoponce/110839994357941432
- * I'm not convinced this approach is flawed. But PR's are welcome!
- * @param {Number} wordCount - The number of words in the passphrase.
+ * Generate a passphrase built up from an acronym (a "backronym").
+ * Rewritten taking ideas from https://github.com/Sc00bz/acronym-passphrase/,
+ * who was inspired by my original approach.
  * @param {Array} wordList - A list of words to choose frome.
  * @param {Boolean} useEntropy - Boolean to use collected entropy.
- * @return {Object} - Dictionary containing the passphrase and its security.
+ * @return {Object} - Dictionary containing the acronym, passphrase, and security.
  */
-function generateAcronym(wordCount, wordList, useEntropy) {
-  var getSecurity = function (entropyList) {
-    return Math.floor(
-      entropyList.reduce((total, a) => total + Math.log2(a), 0)
-    )
+function generateBackronym(wordList, useEntropy) {
+  wordList.sort();
+
+  const _readWords = (words) => {
+    // Break up the massive word list into 2 groups:
+    // 1. by starting letter, ignoring case, "a" through "z".
+    const byLetter = words.reduce((acc, word) => {
+      const firstLetter = word[0].toLowerCase();
+      (acc[firstLetter] = acc[firstLetter] || []).push(word);
+      return acc;
+    }, {});
+
+    // 2. by word length, ordered ascending, 1 character to max
+    const byLength = words.reduce((acc, word) => {
+      const length = word.length;
+      (acc[length] = acc[length] || []).push(word);
+      return acc;
+    }, {});
+
+    // byLetter and byLength need to be iterable
+    return [words.length, Object.values(byLetter), Object.values(byLength)];
   }
 
-  const candidates = []
+  const entropy = getEntropy();
+  const wordStats = _readWords(wordList);
 
-  for (let i = 0; i < wordList.length; i++) {
-    if (wordList[i].length === wordCount) {
-      candidates.push(wordList[i])
-    }
+  const byLetter = wordStats[1];
+  const maxChars = byLetter.length;
+  const byLetterCount = new Array(maxChars);
+
+  const byLength = wordStats[2];
+  const byLengthKeySpace = [];
+
+  // byLetterCount index = word length - 1
+  for (let i = 0; i < maxChars; i++) {
+    byLetterCount[i] = byLetter[i].length;
   }
 
-  const num = secRand(candidates.length, useEntropy)
-  const acronym = candidates[num]
-  const entropies = []
-  const passphraseWords = []
+  // byLength is a two-dimensional array of arrays
+  // group is each individual nested array of increasing word length
+  for (const group of byLength) {
+    let sum = 0n; // BigInt
+    const seen = new Set();
 
-  for(let i = 0; i < acronym.length; i++) {
-    const candidates = []
+    // backronym is each word in an individual array (group)
+    for (const backronym of group) {
+      const word = backronym.toLowerCase();
 
-    for (let j = 0; j < wordList.length; j++) {
-      if (wordList[j][0].toLowerCase() === acronym[i][0].toLowerCase()) {
-        candidates.push("<p>" + wordList[j] + "</p>")
+      if (!seen.has(word)) { // track already processed case-insensitive words
+        let keyspace = 1n; // BigInt
+
+        seen.add(word);
+
+        // char is each individual character in the word
+        for (const char of backronym) {
+          const charIndex = (char.charCodeAt(0) | 32) - 97;
+
+          // keyspace is the total number of combinations for each n-character word
+          // EG, "pass" = count("p") × count("a") × count("s") × count("s")
+          keyspace *= BigInt(byLetterCount[charIndex]);
+        }
+
+        // sum is the total keyspace for each n-character word.
+        // EG, for 4-char words: sum = keyspace("pass") + keyspace("past") + keyspace("pats") + ...
+        sum += keyspace;
       }
     }
 
-    const word = candidates[secRand(candidates.length, useEntropy)]
-    passphraseWords.push(word)
-    entropies.push(candidates.length)
+    // massive numbers, thus BigInt(n) as JavaScript only supports 53 bits
+    byLengthKeySpace.push(sum);
+
+    // no need to calculate further. save unnecessary cpu time
+    if (Math.log2(Number(sum)) >= entropy) {
+      break; // out of group of byLength completely
+    }
   }
 
-  const security = getSecurity(entropies)
-  const passphrase = passphraseWords.join('-')
+  let length;
+  let security;
 
-  return {passphrase, security}
+  // !!! WARNING !!!
+  // the index of byLengthKeySpace tells us our word length
+  // this assumes every index 0 through n is filled without gaps
+  // so i need to be very familiar with my word list
+  // maybe i'll fix this later, but i know what word lists i'm shipping and gaps
+  // don't exist within the entropy contstraints of this project
+  for (const value of byLengthKeySpace) {
+    if (Math.log2(Number(value)) >= entropy) {
+      length = byLengthKeySpace.indexOf(value);
+      security = Math.floor(Math.log2(Number(value)));
+      break;
+    }
+  }
+
+  // !!! WARNING !!!
+  // same as above. beware of gaps if you don't know your word list
+  // pick a random backronym based on the previous length
+  // we know which index to pick by identifying the index in byLengthKeySpace
+  const random = secRand(byLength[length].length, useEntropy);
+  const backronym = byLength[length][random];
+  const passphrase = [];
+
+  // build the passphrase from the chosen backronym
+  // for each character, randomly select a word from the total word list that
+  // starts with that character
+  for (const char of backronym) {
+    const letter = (char.charCodeAt(0) | 32) - 97; 
+    const rand = secRand(byLetter[letter].length, useEntropy);
+    passphrase.push(byLetter[letter][rand]);
+  }
+
+  return {backronym, passphrase, security};
 }
 
 /** Generate a color passphrase */
@@ -211,7 +269,7 @@ function generateColors() {
   const entropy = getEntropy()
   const len = Math.ceil(entropy / Math.log2(colorKeys.length))
 
-  altProps.passId.classList.remove("acronym") // Ensure leading word character is not red
+  altProps.passId.classList.remove("backronym") // Ensure leading word character is not red
   altProps.passId.classList.add("colors")
 
   let pass = generatePass(len, colorKeys, true, altProps.entropyCheck.checked).split(' ')
